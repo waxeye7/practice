642ce37b825e2bdcd71a3c3cb6ec2c0122cf7b53
Merlin Stemmer <stemmermerlin@gmail.com>
2023-05-04 12:09:15 +1200

    Update index.php

--- index.php
+++ index.php
@@ -43,5 +43,54 @@
   ?>
 
 
+<?php
+class User{
+    public $fname;
+    public $lname;
+    public $age;
+    public function __construct($fname,$lname,$age){
+        $this->fname = $fname;
+        $this->lname = $lname;
+        $this->age = $age;
+    }
+    public function initials(){
+        return $this->fname[0] . $this->lname[0];
+    }
+    public function fullname(){
+        return $this->fname . ' ' . $this->lname;
+    }
+} 
+$userA = new User('John','Brown',30);
+$userB = new User('Jane','Doe',20);
+// echo $userA->age;
+class Team{
+    public $memberA;
+    public $memberB;
+    public function __construct($memberA,$memberB){
+        $this->memberA = $memberA;
+        $this->memberB = $memberB;
+    }
+    public function age_average(){
+        return ( $this->memberA->age + $this->memberB->age ) / 2;
+    }
+
+}
+$superduo = new Team($userA,$userB);
+
+echo '<br>';
+
+echo '<br>' . $superduo->age_average();
+echo '<br>' . $userA->initials();
+echo '<br>' . $userA->fullname();
+echo '<br>' . $userA->age;
+echo '<br>' . $userB->fullname();
+echo '<br>' . $userB->age;
+
+
+// :   =>
+// in JS we have : and .  whereas in PHP we have => and ->
+// .   ->
+?>
+
 </body>
 </html>
